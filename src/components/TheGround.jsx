f891eab91e20e2ebe8f47d01008efc1866be23fa
LivingTree <aryarega2811@gmail.com>
2026-09-15 13:12:28 +0700

    feat: add comment because why not

--- src/components/TheGround.jsx
+++ src/components/TheGround.jsx
@@ -3,6 +3,7 @@ import FloatingPlatform from './meshs/FloatingPlatform'
 export default function TheGround() {
   return (
     <group position={[0, 0, 0]}>
+      {/* The Floating Islands */}
       <FloatingPlatform />
     </group>
   )
