ae7501bba27f7e6188d604c3e9eae4dd01fc18e9
LivingTree <aryarega2811@gmail.com>
2026-09-15 13:08:35 +0700

    feat: added Init page with timeout before changing into TheGround

---
+++ src/pages/Init.jsx
@@ -0,0 +1,17 @@
+export default function Init() {
+    return (
+        <div style={{
+            width: '100vw',
+            height: '100vh',
+            backgroundColor: '#000000',
+            color: '#ffffff',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            overflow: 'hidden',
+            fontFamily: 'JetBrains Mono, monospace',
+        }}>
+        </div>
+    )
+}
