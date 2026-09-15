d10af3c6b08ccf6dd82548d72b9885f2e1a612da
LivingTree <aryarega2811@gmail.com>
2026-09-15 10:32:54 +0700

    feat: further refactor by splitting the mesh from TheGround by naming it FloatingPlatform and making it its own file

--- src/components/TheGround.jsx
+++ src/components/TheGround.jsx
@@ -1,22 +1,9 @@
-import { Edges } from '@react-three/drei'
+import FloatingPlatform from './meshs/FloatingPlatform'
 
 export default function TheGround() {
   return (
     <group position={[0, 0, 0]}>
-      {/* The white rectangular platform */}
-      <mesh position={[0, 0, 0]}>
-        <boxGeometry args={[6, 0.4, 4]} />
-        <meshStandardMaterial
-          color="#ffffff"
-          roughness={0.4}
-          metalness={0.1}
-        />
-        {/* Its outline */}
-        <Edges
-          color="#000000"
-          threshold={15}
-        />
-      </mesh>
+      <FloatingPlatform />
     </group>
   )
 }
