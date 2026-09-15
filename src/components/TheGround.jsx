011d10d750c19ce45ee06e12e34d00189fb0d93c
LivingTree <aryarega2811@gmail.com>
2026-09-15 09:04:25 +0700

    feat: Added TheGround, the main 3d platform we will be on

---
+++ src/components/TheGround.jsx
@@ -0,0 +1,22 @@
+import { Edges } from '@react-three/drei'
+
+export default function TheGround() {
+  return (
+    <group position={[0, 0, 0]}>
+      {/* The white rectangular platform */}
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry args={[6, 0.4, 4]} />
+        <meshStandardMaterial
+          color="#ffffff"
+          roughness={0.4}
+          metalness={0.1}
+        />
+        {/* Its outline */}
+        <Edges
+          color="#000000"
+          threshold={15}
+        />
+      </mesh>
+    </group>
+  )
+}
